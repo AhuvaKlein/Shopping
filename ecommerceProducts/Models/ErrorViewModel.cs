8efef206caa1f6487d889f8e7bbfcd75c1967cde
Ahuva Klein <ahuvalklein@gmail.com>
2019-04-21 22:36:43 -0400

    Add project files.

---
+++ ecommerceProducts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ecommerceProducts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
